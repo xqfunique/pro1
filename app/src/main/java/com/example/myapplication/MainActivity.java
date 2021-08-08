054591d8e192f4d6d73f665007958d3f8883e80a
xqfunique <88627550+xqfunique@users.noreply.github.com>
2021-08-08 22:45:25 +0530

    Add files via upload
    new files added

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+
+    }
+}
